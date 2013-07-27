e34f75149374ceb0e7a547577e5d71e1fdb19e09
Philip Schwarz <pschwarz@expedia.com>
2013-07-27 14:52:17 +0100

    call s arraySize

--- -refactor-to-clean-code-kata-java-GeneratePrimes/src/primes/GeneratePrimes.java
+++ -refactor-to-clean-code-kata-java-GeneratePrimes/src/primes/GeneratePrimes.java
@@ -21,34 +21,34 @@ public class GeneratePrimes
         }
         
         // declarations
-        int s = maxValue + 1; // size of array
-        boolean[] f = new boolean[s];
+        int arraySize = maxValue + 1; // size of array
+        boolean[] f = new boolean[arraySize];
         int i;
         // initialize array to true.
-        for (i = 0; i < s; i++)
+        for (i = 0; i < arraySize; i++)
             f[i] = true;
         // get rid of known non-primes
         f[0] = f[1] = false;
         // sieve
         int j;
-        for (i = 2; i < Math.sqrt(s) + 1; i++)
+        for (i = 2; i < Math.sqrt(arraySize) + 1; i++)
         {
             if (f[i]) // if i is uncrossed, cross its multiples.
             {
-                for (j = 2 * i; j < s; j += i)
+                for (j = 2 * i; j < arraySize; j += i)
                     f[j] = false; // multiple is not prime
             }
         }
         // how many primes are there?
         int count = 0;
-        for (i = 0; i < s; i++)
+        for (i = 0; i < arraySize; i++)
         {
             if (f[i])
                 count++; // bump count.
         }
         int[] primes = new int[count];
         // move the primes into the result
-        for (i = 0, j = 0; i < s; i++)
+        for (i = 0, j = 0; i < arraySize; i++)
         {
             if (f[i]) // if prime
                 primes[j++] = i;
