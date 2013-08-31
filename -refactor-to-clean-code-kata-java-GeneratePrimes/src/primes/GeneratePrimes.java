4afc459953a79d1720622969e2d0349de074f843
Philip Schwarz <philip.johann.schwarz@gmail.com>
2013-08-31 18:10:59 +0100

    generate primes

--- -refactor-to-clean-code-kata-java-GeneratePrimes/src/primes/GeneratePrimes.java
+++ -refactor-to-clean-code-kata-java-GeneratePrimes/src/primes/GeneratePrimes.java
@@ -1,6 +1,7 @@
 package primes;
 
 import java.util.Arrays;
+import java.util.function.IntPredicate;
 import java.util.stream.IntStream;
 
 /**
@@ -17,20 +18,33 @@ import java.util.stream.IntStream;
  * uncrossed integer, and cross out all of its multiples.
  * Repeat untilyou have passed the square root of the maximum
  * value.
- *
- * @author Alphonse
- * @version 13 Feb 2002 atp
  */
 public class GeneratePrimes
 {
+    public static int[] generatePrimes(int maxValue)
+    {
+        return integersFrom2UpTo(maxValue)
+                .filter(hasNoDivisorsOtherThanOneAndItself)
+                .toArray();
+    }
+
+    static final IntPredicate hasNoDivisorsOtherThanOneAndItself = n ->
+    {
+        IntPredicate dividesN = m -> n % m == 0;
+
+        return integersFrom2UpTo(n - 1)
+                .filter(dividesN)
+                .count() == 0;
+    };
+
+    private static IntStream integersFrom2UpTo(int n)
+    {
+        return IntStream.rangeClosed(2, n);
+    }
 
     public static void main(String[] args)
     {
         Arrays.stream(generatePrimes(50)).forEach( System.out::println );
     }
-
-    public static int[] generatePrimes(int maxValue)
-    {
-        return IntStream.range(2, maxValue).toArray();
-    }
 }
+
