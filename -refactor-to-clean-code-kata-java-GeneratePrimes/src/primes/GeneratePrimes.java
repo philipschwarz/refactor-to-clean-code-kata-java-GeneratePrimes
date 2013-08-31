7d5ecfafd02ab307d8716e69ad498e66a9cb2825
Philip Schwarz <philip.johann.schwarz@gmail.com>
2013-08-31 14:36:17 +0100

    extract method crossOutMultiples()

--- -refactor-to-clean-code-kata-java-GeneratePrimes/src/primes/GeneratePrimes.java
+++ -refactor-to-clean-code-kata-java-GeneratePrimes/src/primes/GeneratePrimes.java
@@ -18,9 +18,27 @@ public class GeneratePrimes
         if (maxValue < 2) return new int[0];
         
         uncrossIntegersUpTo(maxValue);
-        // get rid of known non-primes
-        crossedOut[0] = crossedOut[1] = true;
-        // sieve
+        
+        crossOutMultiples();
+        
+        // how many primes are there?
+        int count = 0;
+        for (int i = 2; i < crossedOut.length; i++)
+        {
+            if (uncrossed(i))
+                count++; // bump count.
+        }
+        int[] primes = new int[count];
+        // move the primes into the result
+        for (int i = 2, j = 0; i < crossedOut.length; i++)
+        {
+            if (uncrossed(i)) // if prime
+                primes[j++] = i;
+        }
+        return primes; // return the primes
+    }
+
+	private static void crossOutMultiples() {
         for (int i = 2; i < Math.sqrt(crossedOut.length) + 1; i++)
         {
             if (uncrossed(i)) // if i is uncrossed, cross its multiples.
@@ -29,26 +47,11 @@ public class GeneratePrimes
                     crossedOut[j] = true; // multiple is not prime
             }
         }
-        // how many primes are there?
-        int count = 0;
-        for (int i = 0; i < crossedOut.length; i++)
-        {
-            if (uncrossed(i))
-                count++; // bump count.
-        }
-        int[] primes = new int[count];
-        // move the primes into the result
-        for (int i = 0, j = 0; i < crossedOut.length; i++)
-        {
-            if (uncrossed(i)) // if prime
-                primes[j++] = i;
-        }
-        return primes; // return the primes
-    }
+	}
 
 	private static void uncrossIntegersUpTo(int maxValue) {
 		crossedOut = new boolean[maxValue + 1];
-        for (int i = 0; i < crossedOut.length; i++)
+        for (int i = 2; i < crossedOut.length; i++)
             crossedOut[i] = false;
 	}
 
